c8536329e65e6cc76d140de98ac7ee3c60e76b16
Roberdev <r.romerod96@gmail.com>
2026-03-17 16:22:56 -0400

    cloud3

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,7 @@ function App() {
   };
   return (
     <>
-      <main className="main grid grid-cols-1 
-      md:grid-cols-2 xl:grid-cols-3 m-1.5 align-middle gap-10">
+      <main className="main m-1.5 align-middle gap-10 overflow-y-auto">
         <Header filtrar={handleFiltrar} />
         <Pokemones tipoFiltro={selectTipo}/>
         <Targect/>
